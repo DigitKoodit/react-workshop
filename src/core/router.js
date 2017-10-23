30e45f71e1c9abe0e003a66b2f3d065606ba5ec5
Julius Rajala <julius.rajala@futurice.com>
2017-10-23 18:57:45 +0300

    Built step 1 with routing.

---
+++ src/core/router.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const GuildView = ({ guild }) => <h3>Hello {guild ? guild : 'Everyone'}!</h3>;
+
+const Home = () =>
+<div>
+  <h2>Welcome to Kroniikkamaatti v3</h2>
+</div>;
+
+const routes = [
+  { path: '/',
+    component: Home,
+  },
+  { path: '/asteriski',
+    guild: 'Asteriski',
+    component: GuildView,
+  },
+  { path: '/digit',
+    guild: 'Digit',
+    component: GuildView,
+  },
+  { path: '/nucleus',
+    guild: 'Nucleus',
+    component: GuildView,
+  }
+];
+
+const RouteWithSubRoutes = (route) =>
+  <Route exact path={route.path} render={ props => {
+    return <route.component {...props} guild={route.guild} routes={route.routes}/>
+  } }/>
+
+const RouterComponent = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/asteriski">Asteriski</Link></li>
+        <li><Link to="/digit">Digit</Link></li>
+        <li><Link to="/nucleus">Nucleus</Link></li>
+      </ul>
+      <hr/>
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route}/>
+      ))}
+    </div>
+  </Router>
+)
+
+export default RouterComponent;
